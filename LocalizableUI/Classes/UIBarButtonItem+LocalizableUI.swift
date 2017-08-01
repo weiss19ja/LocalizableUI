cb62458099e81013bd3620265d2fd9c9e159cbe1
Jan Weiß <weiss19ja@gmail.com>
2017-08-01 20:53:36 +0200

    Added localizable keys for bar items and textfields

---
+++ LocalizableUI/Classes/UIBarButtonItem+LocalizableUI.swift
@@ -0,0 +1,20 @@
+//
+//  UIBarButtonItem+LocalizableUI.swift
+//  Pods
+//
+//  Created by Jan Weiß on 01.08.17.
+//
+//
+
+import Foundation
+import UIKit
+
+extension UIBarButtonItem {
+    
+    convenience init(localizedKey: String, style: UIBarButtonItemStyle, target: Any?, action: Selector?) {
+        let localizedTitle = LocalizationService.localizedStringFor(localizedKey)
+        self.init(title: localizedTitle, style: style, target: target, action: action)
+        self.localizedKey = localizedKey
+    }
+    
+}
---
+++ LocalizableUI/Classes/UITextField+LocalizableUI.swift
@@ -0,0 +1,39 @@
+//
+//  UITextField+LocalizableUI.swift
+//  Pods
+//
+//  Created by Jan Weiß on 01.08.17.
+//
+//
+
+import Foundation
+import UIKit
+
+private var AssociatedObjectPointer: UInt8 = 0
+
+extension UITextField {
+    
+    // Stores the property of the localized placeholder key
+    @IBInspectable var localizedPlaceholderKey: String? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedObjectPointer) as? String
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedObjectPointer, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            updateLocalizedStrings()
+        }
+    }
+    
+    override func updateLocalizedStrings() {
+        super.updateLocalizedStrings()
+        
+        if let localizedKey = localizedKey {
+            text = LocalizationService.localizedStringFor(localizedKey)
+        }
+        
+        if let localizedPlaceHolderKey = localizedPlaceholderKey {
+            placeholder = LocalizationService.localizedStringFor(localizedPlaceHolderKey)
+        }
+    }
+    
+}
