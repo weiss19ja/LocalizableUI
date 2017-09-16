87425a8974a949a2f9ae56dcf9dc9e95a57b1b3d
Philipp Weiß <philipp307@gmail.com>
2017-09-16 21:25:40 +0200

    adding bundle persistence in the LocalizationManager; Fixing bug in adding new elements to the Hash

--- LocalizableUI/Classes/LocalizationManager.swift
+++ LocalizableUI/Classes/LocalizationManager.swift
@@ -27,6 +27,9 @@ open class LocalizationManager {
     /// The string table to search for localized values
     private var tableName: String?
 
+    /// The bundle to that inclued the table information. Default is main
+    private var bundle: Bundle = Bundle.main
+
     /// private init for the singleton
     /// adding locale changed notification
     private init() {
@@ -50,7 +53,7 @@ open class LocalizationManager {
     internal func add(localizable: Localizable) {
 
         /// return if the object already exists
-        guard weakHash.contains(localizable) else {
+        guard !weakHash.contains(localizable) else {
             return
         }
 
@@ -64,9 +67,9 @@ open class LocalizationManager {
     ///
     /// - Parameters:
     ///   - tableName: The custom tableName or the default one if nil is passed
-    ///   - bundle: Optional Parameter. The bundle which contains the strings table or uses the main bundle if none is passed
+    ///   - bundle: Optional Parameter. The bundle which contains the strings table or uses default bundle from the LocalizationManager
     /// - Throws: A `LocalizableError` if the string table does not exist
-    public func changeLanguage(to tableName: String?, from bundle: Bundle = Bundle.main) throws {
+    public func changeLanguage(to tableName: String?, from bundle: Bundle = LocalizationManager.sharedInstance().bundle) throws {
         guard let filePath = bundle.path(forResource: tableName, ofType: "strings"),
             FileManager.default.fileExists(atPath: filePath) == true else {
 
@@ -74,7 +77,9 @@ open class LocalizationManager {
         }
 
         self.tableName = tableName
-        weakHash.allObjects.flatMap { $0 as? Localizable}.forEach { $0.updateLocalizedStrings() }
+        self.bundle = bundle
+
+        languageChanged()
     }
 
     /// Method to get the right Localization for a Key that is included in the localized.strings.
@@ -82,12 +87,13 @@ open class LocalizationManager {
     ///
     /// - Parameters:
     ///   - key: key
-    ///   - bundle: bundle name
+    ///   - bundle: bundle -> default bundle stored in the LocalizationManager -> Default main.
     ///   - value: value -> default ""
     ///   - comment: comment -> default ""
     /// - Returns: The localized string
-    static func localizedStringFor(_ key: String, bundle: Bundle = Bundle.main, value: String = "", comment: String = "") -> String {
+    static func localizedStringFor(_ key: String, bundle: Bundle = LocalizationManager.sharedInstance().bundle, value: String = "", comment: String = "") -> String {
         let tableName = _localizationManager?.tableName
+
         return NSLocalizedString(key, tableName: tableName, bundle: bundle, value: value, comment: comment)
     }
     
